2290e4fe1edf50405400dc596320a1942cea8f21
simojenki <simojenki@users.noreply.github.com>
2020-01-21 07:30:48 +1100

    Using shell=True when invoking subprocess.call

--- scdl/scdl.py
+++ scdl/scdl.py
@@ -485,7 +485,7 @@ def download_original_file(track, title):
         new = shlex.quote(newfilename)
         old = shlex.quote(filename)
         logger.debug("ffmpeg -i {0} {1} -loglevel fatal".format(old, new))
-        subprocess.call(["ffmpeg", "-i", old, new, "-loglevel", "fatal"])
+			  subprocess.call("ffmpeg -i {0} {1} -loglevel fatal".format(old, new), shell=True)
         os.remove(filename)
         filename = newfilename
 
@@ -515,8 +515,11 @@ def download_hls_mp3(track, title):
 
     # Get the requests stream
     url = get_track_m3u8(track)
-    res = subprocess.call(
-				["ffmpeg", "-i", '"' + url + '"', "-c", "copy", '"' + filename + '"', "-loglevel", "fatal"]
+		res = subprocess.call(
+        "ffmpeg -i {0} -c copy {1} -loglevel fatal".format(
+            '"' + url + '"',
+            '"' + filename + '"'
+        ), shell=True
     )
     return filename
 
